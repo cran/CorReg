8c7f69b04d8bab38e51bb012492ebaf73e688cd0
Clement Thery <corregeous@correg.org>
2020-02-20 14:00:05 +0000

    version 1.2.17

--- R/readY.R
+++ R/readY.R
@@ -4,20 +4,20 @@
 # ' @param X the dataset (named) if labels is null
 # ' @param intercept boolean defining wether A contains an intercept or not
 # ' @param ANOVA boolean to add Anova test for each coefficient
-# ' @param print boolean to print ANOVA if computed
+# ' @param verbose boolean to print ANOVA if computed
 # ' @param Y the response variable if ANOVA is computed
 # '@export
-readY<-function(A=A,labels=NULL,X=NULL,intercept=TRUE,ANOVA=FALSE,print=FALSE,Y=NULL){
-   if(is.null(labels)){
-      labels=names(X)
+readY <- function(A = A, labels = NULL, X = NULL, intercept = TRUE, ANOVA = FALSE, verbose = FALSE, Y = NULL) {
+   if (is.null(labels)) {
+      labels = names(X)
    }
-   if(intercept){labels=c("intercept",labels)}
-   interp=cbind(A[A!=0],labels[A!=0])
-   if(ANOVA & !is.null(X) & !is.null(Y)){
-      Xred=X[,A[-intercept]!=0]
-      reslm=lm(Y~.,data=data.frame(Xred))
-      interp=summary(reslm) 
+   if (intercept) {labels = c("intercept", labels)}
+   interp = cbind(A[A != 0], labels[A != 0])
+   if (ANOVA & !is.null(X) & !is.null(Y)) {
+      Xred = X[, A[-intercept] != 0]
+      reslm = lm(Y ~ ., data = data.frame(Xred))
+      interp = summary(reslm)
    }
-   if(print){print(interp)}
+   if (verbose) {print(interp)}
    return(interp)
 }
